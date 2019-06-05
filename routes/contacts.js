5523b089da684d93d24b42463a0a9d182d202ee6
aidyn altay <bover199131@gmail.com>
2019-06-05 16:00:03 +0600

    Server Setup with routes

---
+++ routes/contacts.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const router = express.Router();
+
+// @route  Get api/contacts;
+// @desc Get All Users Contacts;
+// @access Private;
+router.get("/", (req, res) => {
+  res.send("Get all contacts");
+});
+
+// @route  POST api/contacts;
+// @desc Add new contacts;
+// @access Private;
+router.post("/", (req, res) => {
+  res.send("Add contact");
+});
+
+// @route  PUT api/contacts/:id;
+// @desc Update contacts;
+// @access Private;
+router.put("/:id", (req, res) => {
+  res.send("Update Contact");
+});
+
+// @route  DELETE api/contacts/:id;
+// @desc Update contacts;
+// @access Private;
+router.delete("/:id", (req, res) => {
+  res.send("Delete Contact");
+});
+
+module.exports = router;
